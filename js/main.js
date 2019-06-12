e74226388ad70f7b14430b774d0cf6c5d4f4b023
Max Sof <maxeinsof@gmail.com>
2019-06-13 01:52:45 +0700

    Добавляет функции создания элементов пинов и их добавления в DOM-дерево

--- js/main.js
+++ js/main.js
@@ -30,3 +30,27 @@ function generateData() {
 }
 
 var data = generateData();
+
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var PIN_WIDTH = 50;
+var PIN_HEIGHT = 70;
+var mapPins = document.querySelector('.map__pins');
+
+function generatePin(similarAd) {
+  var pin = pinTemplate.cloneNode(true);
+  pin.style = 'left: ' + (similarAd.location.x - (PIN_WIDTH / 2)) + 'px; top: ' + (similarAd.location.y - PIN_HEIGHT) + 'px;';
+  pin.children[0].src = similarAd.author.avatar;
+  pin.children[0].alt = 'Заголовок объявления';
+  return pin;
+}
+
+function addPins() {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < data.length; i++) {
+    fragment.appendChild(generatePin(data[i]));
+  }
+  mapPins.appendChild(fragment);
+}
+
+addPins();
+
