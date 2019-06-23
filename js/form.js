7335f1886777eb9c0981d507efb6863fa60d4424
Max Sof <maxeinsof@gmail.com>
2019-06-23 20:51:20 +0700

    Разделяет код на модули

---
+++ js/form.js
@@ -0,0 +1,48 @@
+'use strict';
+
+(function () {
+  var adForm = document.querySelector('.ad-form');
+  var adFormInputs = adForm.querySelectorAll('input, select, textarea');
+  var addressInput = adForm.querySelector('#address');
+  var adPriceInput = adForm.querySelector('#price');
+  var adTypeSelect = adForm.querySelector('#type');
+  var minPrice = {
+    bungalo: 0,
+    flat: 1000,
+    house: 5000,
+    palace: 10000
+  };
+  var adFormTime = adForm.querySelector('.ad-form__element--time');
+  var timeInSelect = adForm.querySelector('#timein');
+  var timeOutSelect = adForm.querySelector('#timeout');
+
+  window.utility.disableInputs(adFormInputs);
+
+  adTypeSelect.addEventListener('click', onTypeSelectClick);
+  adFormTime.addEventListener('click', onAdFormTimeClick);
+
+  function activateAdForm() {
+    adForm.classList.remove('ad-form--disabled');
+    window.utility.enableInputs(adFormInputs);
+  }
+
+  function setAddress(pinCoords) {
+    addressInput.value = pinCoords.x + ', ' + pinCoords.y;
+  }
+
+  function onTypeSelectClick() {
+    var minValue = minPrice[adTypeSelect.value];
+    adPriceInput.min = minValue;
+    adPriceInput.placeholder = minValue;
+  }
+
+  function onAdFormTimeClick(evt) {
+    var changedSelect = evt.target === timeInSelect ? timeOutSelect : timeInSelect;
+    changedSelect.value = evt.target.value;
+  }
+
+  window.form = {
+    activateAdForm: activateAdForm,
+    setAddress: setAddress
+  };
+})();
