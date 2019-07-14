ab01965a870b53aa52b6ecb0910321eaacc03f19
Max Sof <maxeinsof@gmail.com>
2019-07-15 01:50:11 +0700

    Перемещает обработчики и функции закрытия карточки в модуль карточки, добавляет функцию удаления пинов

--- js/generateCard.js
+++ js/generateCard.js
@@ -3,6 +3,7 @@
 (function () {
   var PHOTO_WIDTH = 45;
   var PHOTO_HEIGHT = 40;
+  var ESC_KEYCODE = 27;
   var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
   var housingTypeMap = {
     'flat': 'Квартира',
@@ -10,31 +11,53 @@
     'house': 'Дом',
     'palace': 'Дворец'
   };
+  var cardElement = null;
 
-  function generateCard(similarAd, callback) {
-    var card = cardTemplate.cloneNode(true);
-    card.querySelector('.popup__title').textContent = similarAd.offer.title;
-    card.querySelector('.popup__text--address').textContent = similarAd.offer.address;
-    card.querySelector('.popup__text--price').textContent = similarAd.offer.price + '₽/ночь';
-    card.querySelector('.popup__type').textContent = housingTypeMap[similarAd.offer.type];
-    card.querySelector('.popup__text--capacity').textContent = similarAd.offer.rooms + ' комнаты для ' + similarAd.offer.guests + ' гостей';
-    card.querySelector('.popup__text--time').textContent = 'Заезд после ' + similarAd.offer.checkin + ', выезд до ' + similarAd.offer.checkout;
-    card.querySelector('.popup__description').textContent = similarAd.offer.description;
-    card.querySelector('.popup__avatar').src = similarAd.author.avatar;
+  function generateCard(similarAd) {
+    cardElement = generateCardElement(similarAd);
+
+    cardElement.querySelector('.popup__close').addEventListener('click', function () {
+      closeCard();
+    });
+    document.addEventListener('keydown', onCardEscPress);
+
+    return {
+      element: cardElement,
+      close: closeCard
+    };
+  }
+
+  function onCardEscPress(evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      closeCard();
+    }
+  }
+
+  function closeCard() {
+    cardElement.remove();
+    document.removeEventListener('keydown', onCardEscPress);
+  }
+
+  function generateCardElement(similarAd) {
+    var cardElement = cardTemplate.cloneNode(true);
+    cardElement.querySelector('.popup__title').textContent = similarAd.offer.title;
+    cardElement.querySelector('.popup__text--address').textContent = similarAd.offer.address;
+    cardElement.querySelector('.popup__text--price').textContent = similarAd.offer.price + '₽/ночь';
+    cardElement.querySelector('.popup__type').textContent = housingTypeMap[similarAd.offer.type];
+    cardElement.querySelector('.popup__text--capacity').textContent = similarAd.offer.rooms + ' комнаты для ' + similarAd.offer.guests + ' гостей';
+    cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + similarAd.offer.checkin + ', выезд до ' + similarAd.offer.checkout;
+    cardElement.querySelector('.popup__description').textContent = similarAd.offer.description;
+    cardElement.querySelector('.popup__avatar').src = similarAd.author.avatar;
 
     similarAd.offer.features.forEach(function (name) {
-      card.querySelector('.popup__features').appendChild(addFeature(name));
+      cardElement.querySelector('.popup__features').appendChild(addFeature(name));
     });
 
     similarAd.offer.photos.forEach(function (src) {
-      card.querySelector('.popup__photos').appendChild(addPhoto(src));
+      cardElement.querySelector('.popup__photos').appendChild(addPhoto(src));
     });
 
-    card.querySelector('.popup__close').addEventListener('click', function () {
-      callback();
-    });
-
-    return card;
+    return cardElement;
   }
 
   function addFeature(name) {
--- js/map.js
+++ js/map.js
@@ -1,10 +1,10 @@
 'use strict';
 
 (function () {
-  var ESC_KEYCODE = 27;
   var map = document.querySelector('.map');
   var mapPins = map.querySelector('.map__pins');
   var mapFiltersElement = map.querySelector('.map__filters-container');
+  var pins = [];
   var activeCard = null;
 
   function activateMap() {
@@ -19,11 +19,18 @@
     return !map.classList.contains('map--faded');
   }
 
-  function addPins(array) {
-    var oldPins = mapPins.querySelectorAll('.map__pin:not(.map__pin--main)');
-    oldPins.forEach(function (oldPin) {
-      oldPin.remove();
+  function removePins() {
+    pins.forEach(function (pin) {
+      pin.remove();
     });
+
+    pins = [];
+  }
+
+  function addPins(array) {
+    if (+pins.length) {
+      removePins();
+    }
 
     var fragment = document.createDocumentFragment();
 
@@ -34,6 +41,8 @@
         addCard(object);
       });
 
+      pins.push(newPin);
+
       fragment.appendChild(newPin);
     });
 
@@ -42,25 +51,11 @@
 
   function addCard(object) {
     if (activeCard) {
-      closeCard();
+      activeCard.close();
     }
 
-    activeCard = window.generateCard(object, closeCard);
-
-    document.addEventListener('keydown', onCardEscPress);
-
-    map.insertBefore(activeCard, mapFiltersElement);
-  }
-
-  function onCardEscPress(evt) {
-    if (evt.keyCode === ESC_KEYCODE) {
-      closeCard();
-    }
-  }
-
-  function closeCard() {
-    activeCard.remove();
-    document.removeEventListener('keydown', onCardEscPress);
+    activeCard = window.generateCard(object);
+    map.insertBefore(activeCard.element, mapFiltersElement);
   }
 
   window.map = {
